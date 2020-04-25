782ef85570eb791678a494c9ed3d350e392e6e60
Bigmax81 <52238509+Bigmax81@users.noreply.github.com>
2020-04-25 18:07:40 +0200

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -2,7 +2,7 @@
  * <b>Description : </b> Projet ayant pour objectif la mise en place du jeu de l'infection ainsi que l'implémentation
  * d'algorithmes d'Intelligence Artificielle tels le MiniMax, l'élagage AlphaBêta ou encore leur version Négamax.
  * 
- * @author Emmanuel Garreau 21700336
+ * @author Emmanuel Garreau
  * 
  * @version V1
  * 
